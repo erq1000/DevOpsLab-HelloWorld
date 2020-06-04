645a79f5c39bba1315bf21eb4650e2b8bdabde52
erq1000 <erq1000@hotmail.com>
2020-06-04 18:21:08 -0400

    teste 2

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "devolps-projeto1.")
+        self.assertRegex(result.data.decode(), "Devolps - projeto1.")
 
 
 if __name__ == "__main__":
